a778b3ad9a297b2ecdb21721772861e6d393151e
Meu Perfil <Meu Perfil@10.131.75.43>
2017-10-18 16:03:25 -0200

    Enviar.php funcionando corretamente :3

--- Receber.php
+++ Receber.php
@@ -2,7 +2,7 @@
 
 error_reporting(~E_WARNING);
 
-$server = '192.168.0.108';
+$server = '127.0.0.1';
 $port = 8888;
 
 if (!($sock = socket_create(AF_INET, SOCK_DGRAM, 0))) {
@@ -11,12 +11,30 @@ if (!($sock = socket_create(AF_INET, SOCK_DGRAM, 0))) {
 
     die("Couldn't create socket: [$errorcode] $errormsg \n");
 }
-
 echo "Socket created \n";
-
-    echo 'esperando';
-    //$r = socket_recvfrom($sock, $buf, $port, 0, $remote_ip, $remote_port);
-    $funciona= socket_recvfrom($sock, $buf, 1, 0, $port );
-    var_dump($buf);
-    echo $funciona;    
- 
+$i = 0;
+    while ($i==0){
+        //$r = socket_recvfrom($sock, $buf, $port, 0, $remote_ip, $remote_port);
+        
+    $bind = socket_bind($sock, $server,1220);
+    var_dump($bind);
+    if ($bind != false){
+        $funciona= socket_recvfrom($sock, $buf, 100, 0, $port);
+        if ($funciona !=0){
+    
+            var_dump($funciona);
+            var_dump($buf);
+             echo $funciona;
+            $i++;
+        }
+    }
+    else{
+        $errorcode = socket_last_error();
+        $errormsg = socket_strerror($errorcode);
+        die("Não consegui realizar o bind: $errorcode, $errormsg");
+    }
+    
+    
+    
+     
+    }
--- enviar.php
+++ enviar.php
@@ -19,13 +19,24 @@ var_dump($codaprendido);
 $idmodelo = $dados ['MOD_ID'];
 var_dump($idmodelo);
 $server_ip = '192.168.0.108';
-$server_port = 8889;
+$server_port = 8898;
 $beat_period = 5;
 $message = explode(",",$codaprendido);
 //var_dump($message);
 
 $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+$cSinal = false;
+$fSinal = false;
+if($cSinal == false){
+    $cSinal = true;
+    socket_sendto($socket, "[", 1, 0, $server_ip, $server_port);
+}
 for ($i = 0; $i <= 196; $i++) {
     
     socket_sendto($socket, $message[$i], 4, 0, $server_ip, $server_port);
+    sleep(0.1);
+}
+if($fSinal == false){
+    $fSinal = true;
+    socket_sendto($socket,"]", 1, 0, $server_ip, $server_port);
 }
