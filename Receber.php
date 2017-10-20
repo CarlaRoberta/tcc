3b01b34e9e564a2dbc97f5e26c38ca8d60fef937
Meu Perfil <Meu Perfil@carla>
2017-10-20 11:55:06 -0200

    foi+-

---
+++ Receber.php
@@ -0,0 +1,15 @@
+<html><h1>Server</h1></html>
+<?php
+error_reporting(E_ALL | E_STRICT);
+
+$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+
+socket_bind($socket, '192.168.0.105', 8888);
+
+$from = "";
+$port = 8888;
+while(1){
+socket_recvfrom($socket, $buf, 12, 0, $from, $port);
+echo "Received $buf ";
+}
+?>
